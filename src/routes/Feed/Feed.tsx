0725972d8ca7267153403036183cc1c488217a9f
Lix3nn53 <lixakyildiz@gmail.com>
2020-10-30 18:54:55 +0300

    Work on css color variables

--- src/routes/Feed/Feed.tsx
+++ src/routes/Feed/Feed.tsx
@@ -11,6 +11,9 @@ const Feed: FunctionComponent = () => (
     </div>
     <div className="feed-sidebar-1">Sidebar1</div>
     <div className="feed-sidebar-2">Sidebar2</div>
+    <div className="feed-bottom-1">Bottom1</div>
+    <div className="feed-bottom-2">Bottom2</div>
+    <div className="feed-bottom-3">Bottom3</div>
   </div>
 );
 
