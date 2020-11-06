f8e5ea622143a09a179831e23861d8fefa94ca8b
Lix3nn53 <lixakyildiz@gmail.com>
2020-11-06 12:31:20 +0300

    Input type checkbox and radio styling

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -7,7 +7,7 @@ import Button from '../Button';
 type InputData = {
   id: string;
   name: string;
-  type: 'text' | 'password' | 'email';
+  type: 'text' | 'password' | 'email' | 'checkbox' | 'radio';
 };
 
 type Props = JSX.IntrinsicElements['form'] & {
@@ -28,17 +28,36 @@ const Form: FC<Props> = ({
   const baseStyle = 'form';
   const classNames = className ? `${baseStyle}  ${className}` : `${baseStyle}`;
 
-  const createInput = (inputData: InputData, rowIndex: number, inputIndex: number) => {
+  const createInput = (inputData: InputData) => {
     const { id, name, type } = inputData;
 
+    if (type === 'checkbox' || type === 'radio') {
+      const markType = type === 'checkbox' ? 'crossmark' : 'circle';
+
+      return (
+        <label className="input-box-wrap" htmlFor={id}>
+          {name}
+          {/* formNoValidate disables default css of browser */}
+          <input formNoValidate id={id} type={type} name={name} />
+          <span className={`mark ${markType}`} />
+        </label>
+      );
+    }
+
+    return (
+      <label htmlFor={id}>
+        {name}
+        {/* formNoValidate disables default css of browser */}
+        <input formNoValidate id={id} type={type} name={name} />
+      </label>
+    );
+  };
+
+  const createFormItem = (inputData: InputData, rowIndex: number, inputIndex: number) => {
     return (
       <div key={rowIndex} className="form-item" style={{ flex: '1' }}>
         <div key={inputIndex} className="form-input">
-          <label htmlFor={id}>
-            {name}
-            {/* formNoValidate disables default css of browser */}
-            <input formNoValidate id={id} type={type} name={name} />
-          </label>
+          {createInput(inputData)}
         </div>
       </div>
     );
@@ -49,7 +68,7 @@ const Form: FC<Props> = ({
 
     rowArray.forEach((inputArray, rowIndex) => {
       inputArray.forEach((inputData, inputIndex) => {
-        rows.push(createInput(inputData, rowIndex, inputIndex));
+        rows.push(createFormItem(inputData, rowIndex, inputIndex));
       });
     });
 
--- src/routes/Login/Login.tsx
+++ src/routes/Login/Login.tsx
@@ -11,6 +11,9 @@ const Login: FunctionComponent = () => (
         rowArray={[
           [{ id: 'login-email', name: 'Email', type: 'email' }],
           [{ id: 'login-password', name: 'Password', type: 'password' }],
+          [{ id: 'login-remember', name: 'Remember Me', type: 'checkbox' }],
+          [{ id: 'login-remember2', name: 'radyo', type: 'radio' }],
+          [{ id: 'login-remember3', name: 'radyo', type: 'radio' }],
         ]}
         buttonText="Login Now!"
         onSubmit={(event) => {
