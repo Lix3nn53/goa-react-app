8606859fe89ce74c988840957a7d3ff293a814a0
Lix3nn53 <lixakyildiz@gmail.com>
2021-05-03 08:33:07 +0300

    Add discord frame

--- src/routes/Home/Home.tsx
+++ src/routes/Home/Home.tsx
@@ -68,15 +68,15 @@ const Home: FunctionComponent = () => (
       </div>
 
       <div className="sidebar flex-container column">
-        <Card header={<p>Title</p>}>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
-            dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-            mollit anim id est laborum.
-          </p>
+        <Card>
+          <iframe
+            src="https://discord.com/widget?id=452631187010093066&theme=dark"
+            width="350"
+            height="500"
+            frameBorder="0"
+            sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"
+            title="discord-iframe"
+          />
         </Card>
         <Card header={<p>Title</p>}>
           <p>
