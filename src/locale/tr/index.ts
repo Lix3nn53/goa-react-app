73ad430366c4ed2f41fe1b7b95d3d904ff65e8e0
Lix3nn53 <lixakyildiz@gmail.com>
2021-05-05 16:54:07 +0300

    Tr locale

--- src/locale/tr/index.ts
+++ src/locale/tr/index.ts
@@ -1,9 +1,16 @@
 import header from './header.json';
+
+import attribute from './attribute.json';
+import classes from './classes.json';
 import element from './element.json';
+import item from './item.json';
 
 export default {
   translation: {
     header,
+    attribute,
+    classes,
     element,
+    item,
   },
 };
