9a4a287da5e850433527abc071b0fb3a67181dbc
Lix3nn53 <lixakyildiz@gmail.com>
2021-05-07 21:37:17 +0300

    Work on weapons guide

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -4,11 +4,25 @@ import './Table.css';
 
 export type Props = JSX.IntrinsicElements['table'] & {
   cells: Array<Array<ReactNode>>;
+  headers?: Array<ReactNode>;
 };
 
-const Table: FC<Props> = ({ className, children, style, cells }) => {
+const Table: FC<Props> = ({ className, children, style, cells, headers }) => {
   const baseStyle = 'table';
   const classNames = className ? `${baseStyle}  ${className}` : `${baseStyle}`;
+
+  const renderHeaders = () => {
+    if (!headers) return <div />;
+
+    const list: Array<ReactNode> = [];
+
+    headers.forEach((header, index) => {
+      /* eslint-disable-next-line react/no-array-index-key */
+      list.push(<th key={`tr${index}`}>{header}</th>);
+    });
+
+    return list;
+  };
 
   const renderCells = () => {
     const list: Array<ReactNode> = [];
@@ -29,6 +43,7 @@ const Table: FC<Props> = ({ className, children, style, cells }) => {
   return (
     <table className={classNames} style={style}>
       <tbody>
+        {renderHeaders()}
         {renderCells()}
         {children}
       </tbody>
--- src/routes/Guide/Pages/SubPages/Items/Intro.tsx
+++ src/routes/Guide/Pages/SubPages/Items/Intro.tsx
@@ -3,47 +3,51 @@ import { useTranslation } from 'react-i18next';
 
 import Gallery from '../../../../../components/Gallery';
 
+import './Items.css';
+
 const ClassesIntro: FunctionComponent = () => {
   const { t } = useTranslation();
 
   return (
     <div className="guide-page flex-container row-reverse">
       <div style={{ flex: '10' }}>
-        <p>{t('classes.guide.p0')}</p>
+        <p>{t('item.guide.p0')}</p>
         <h2>
-          <span>{t('classes.guide.h1')}</span>
+          <span>{t('item.guide.h1')}</span>
         </h2>
-        <p>{t('classes.guide.p1')}</p>
+        <p>
+          {t('item.guide.p1')}
+          <a href="https://sketchfab.com/Lix3nn/models" target="_blank" rel="noreferrer">
+            https://sketchfab.com/Lix3nn/models
+          </a>
+        </p>
+        <h2 style={{ paddingBottom: '16px' }}>
+          <span>Gallery</span>
+        </h2>
         <Gallery
           pictures={[
             {
-              src:
-                'https://cdn.discordapp.com/attachments/689660026423345188/839467612349005844/unknown.png',
-              name: 'Step 1: Open the Menu',
-              description: 'Step 1: Open the Menu',
+              src: 'https://media.giphy.com/media/1mhU3YqvhgLv2utfKv/giphy.gif',
+              name: 'Great  Sword',
+              description: 'Great  Sword',
             },
             {
-              src:
-                'https://cdn.discordapp.com/attachments/689660026423345188/839470177640906782/unknown.png',
-              name: 'Step 2: Click on Character',
-              description: 'Step 2: Click on Character',
+              src: 'https://media.giphy.com/media/8YKZGnI8Eb6qD83tLa/giphy.gif',
+              name: 'Staff',
+              description: 'Staff',
             },
             {
-              src:
-                'https://cdn.discordapp.com/attachments/689660026423345188/839467838255267890/unknown.png',
-              name: 'Step 3: Click on Class',
-              description: 'Step 3: Click on Class',
+              src: 'https://media.giphy.com/media/5nbDogvtGpAtQBsaMF/giphy.gif',
+              name: 'Wand',
+              description: 'Wand',
             },
             {
-              src:
-                'https://cdn.discordapp.com/attachments/689660026423345188/839467967615074304/unknown.png',
-              name: 'Step 4: Select Class Tier',
-              description: 'Step 4: Select Class Tier',
+              src: 'https://media.giphy.com/media/QZVdlEQuLzxLlTdCGl/giphy.gif',
+              name: 'Spear',
+              description: 'Spear',
             },
           ]}
         />
-        <h2>{t('classes.guide.h2')}</h2>
-        <p>{t('classes.guide.p2')}</p>
       </div>
     </div>
   );
