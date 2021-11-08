4d5f054435166143bab0d4c47ef8039631361da6
Lix3nn53 <lixakyildiz@gmail.com>
2021-11-09 01:01:16 +0300

    complete merchants guide page

--- src/routes/Guide/Pages/SubPages/NPC/Merchants.tsx
+++ src/routes/Guide/Pages/SubPages/NPC/Merchants.tsx
@@ -16,6 +16,12 @@ const Merchants: FunctionComponent = () => {
           pictures={[
             {
               src:
+                'https://cdn.discordapp.com/attachments/689660026423345188/907342252471377930/unknown.png',
+              name: 'A Weaponsmith NPC',
+              description: 'A Weaponsmith NPC, right click to interract.',
+            },
+            {
+              src:
                 'https://cdn.discordapp.com/attachments/689660026423345188/907208264121475102/unknown.png',
               name: 'Sell Items',
               description: 'Click to sell items from your inventory.',
@@ -85,6 +91,75 @@ const Merchants: FunctionComponent = () => {
                 'https://cdn.discordapp.com/attachments/689660026423345188/907211588522635304/unknown.png',
               name: 'Enchant Items',
               description: 'Click to enchant items.',
+            },
+          ]}
+        />
+        <h2>
+          <span>{t('npc.guide.merchant.h3')}</span>
+        </h2>
+        <p>{t('npc.guide.merchant.p3')}</p>
+        <Gallery
+          size="small"
+          pictures={[
+            {
+              src:
+                'https://cdn.discordapp.com/attachments/689660026423345188/907215095296307220/unknown.png',
+              name: 'Personal Storage',
+              description: 'Click to open personal storage.',
+            },
+            {
+              src:
+                'https://cdn.discordapp.com/attachments/689660026423345188/907215131128242216/unknown.png',
+              name: 'Guild Storage',
+              description: 'Click to open guild storage.',
+            },
+            {
+              src:
+                'https://cdn.discordapp.com/attachments/689660026423345188/907215163034333194/unknown.png',
+              name: 'Bazaar Storage',
+              description: 'Click to open bazaar storage.',
+            },
+            {
+              src:
+                'https://cdn.discordapp.com/attachments/689660026423345188/907215195653410836/unknown.png',
+              name: 'Premium Storage',
+              description: 'Click to open premium storage.',
+            },
+          ]}
+        />
+        <h2>
+          <span>{t('npc.guide.merchant.h4')}</span>
+        </h2>
+        <p>{t('npc.guide.merchant.p4')}</p>
+        <Gallery
+          size="small"
+          pictures={[
+            {
+              src:
+                'https://cdn.discordapp.com/attachments/689660026423345188/907214989268492288/unknown.png',
+              name: 'Buy Gathering Tools',
+              description: 'Click to but gathering tools.',
+            },
+            {
+              src:
+                'https://cdn.discordapp.com/attachments/689660026423345188/907215019186462720/unknown.png',
+              name: 'Buy Utility Items',
+              description: 'Click to utility items.',
+            },
+          ]}
+        />
+        <h2>
+          <span>{t('npc.guide.merchant.h5')}</span>
+        </h2>
+        <p>{t('npc.guide.merchant.p5')}</p>
+        <Gallery
+          size="small"
+          pictures={[
+            {
+              src:
+                'https://cdn.discordapp.com/attachments/689660026423345188/907214900034674758/unknown.png',
+              name: 'Buy Potions',
+              description: 'Click to buy potions.',
             },
           ]}
         />
