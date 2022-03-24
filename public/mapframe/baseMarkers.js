8d969cbf7d676a112b3e287ad57e3fa94b43759c
Lix3nn53 <lixakyildiz@gmail.com>
2022-03-24 15:55:20 +0300

    Remove map files, deploy to another site instead

--- public/mapframe/baseMarkers.js
+++
@@ -1,4 +0,0 @@
-overviewer.util.injectMarkerScript('markersDB.js');
-overviewer.util.injectMarkerScript('markers.js');
-overviewer.util.injectMarkerScript('regions.js');
-overviewer.collections.haveSigns=true;
--- src/routes/Map/Map.tsx
+++ src/routes/Map/Map.tsx
@@ -5,7 +5,7 @@ import './Map.css';
 
 const Map: FunctionComponent = () => (
   <div className="map-frame-container flex-container column">
-    <iframe className="map-frame" title="map" src="./mapframe/index.html" />
+    <iframe className="map-frame" title="map" src="https://master.d2esxhypr1dlpv.amplifyapp.com" />
   </div>
 );
 
